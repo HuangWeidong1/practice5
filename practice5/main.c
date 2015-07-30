6e9403166b7750e698e2fb2174dbe5ed14ecb917
中软mini025 <zhongruanmini025@zhongruanmini025deMac-mini.local>
2015-07-29 21:06:03 -0700

    Initial Commit

---
+++ practice5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  practice5
+//
+//  Created by 中软mini025 on 15/7/29.
+//  Copyright (c) 2015年 中软mini025. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
